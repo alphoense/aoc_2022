2b632d1a169ed7b500b69a8065a736c8f723ee27
Barbara Soltysiak <barbara.soltysiak@zendesk.com>
2022-12-03 22:35:14 +0100

    simple modification

--- aoc_day_3/aoc_day_3.py
+++ aoc_day_3/aoc_day_3.py
@@ -8,15 +8,15 @@ from helpers.day import Day
 class Day3(Day):
     val_str = string.ascii_lowercase + string.ascii_uppercase
 
-    def call(self, example_str: str):
-        first_halp = example_str[:int(len(example_str) / 2)]
-        second_half = example_str[int(len(example_str) / 2):]
-        return min(set(first_halp) & set(second_half))
+    def call(self, character):
+        return self.val_str.index(character) + 1
 
     def task_1(self):
         sum = 0
         for item in self.file_content:
-            sum += self.val_str.index(self.call(example_str=item)) + 1
+            first_halp = item[:int(len(item) / 2)]
+            second_half = item[int(len(item) / 2):]
+            sum += self.call(min(set(first_halp) & set(second_half)))
         return sum
 
     def task_2(self):
@@ -27,7 +27,7 @@ class Day3(Day):
 
         for item in chunked_list:
             character = min(set(item[0]) & set(item[1]) & set(item[2]))
-            sum += self.val_str.index(character) + 1
+            sum += self.call(character)
 
         return sum
 
@@ -41,4 +41,3 @@ aoc_day_input = Day3("input.txt")
 print(aoc_day_input.task_1())
 
 print(aoc_day_input.task_2())
-
