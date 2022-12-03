f5c28bf84ef9f8d9d1792e2f7b65c1d1c3088f55
Barbara Soltysiak <barbara.soltysiak@zendesk.com>
2022-12-03 22:36:53 +0100

    add d04

---
+++ aoc_day_4/aoc_day_4.py
@@ -0,0 +1,22 @@
+from helpers.day import Day
+
+
+class Day4(Day):
+    def call(self):
+        pass
+
+    def task_1(self):
+        pass
+
+    def task_2(self):
+        pass
+
+
+aoc_day = Day4()
+print("----test data-----")
+print(aoc_day.task_1())
+print(aoc_day.task_2())
+# print("----input data-----")
+# aoc_day_input = Day4("input.txt")
+# print(aoc_day_input.task_1())
+# print(aoc_day_input.task_2())
